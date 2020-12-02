28087ad404427892456ae10a8aad9aa994352fc6
Bowie <kevinbowie02@gmail.com>
2020-12-02 17:05:15 +0700

    first

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Post.css";
+
+function Post() {
+  return (
+    <div className="post">
+      <h3>Username</h3>
+      <img className="post_image" src="" alt="" />
+
+      <h4 className="post_text">
+        <strong>kevinbowie</strong> Welcome to my instagram post
+      </h4>
+    </div>
+  );
+}
+
+export default Post;
